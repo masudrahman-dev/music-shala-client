a6cf23289fc51ad9d2db32c4450388910e54ee1d
masudrahman-dev <masudrahman.dev@gmail>
2023-06-07 00:17:15 +0600

    project setup done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import AuthProvider from "./contexts/AuthProvider";
+import Main from "./layout/Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <Main />
     </AuthProvider>
   </React.StrictMode>
 );
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Main = () => {
-    return (
-        <div>
-            main
-        </div>
-    );
+  return <div className="text-5xl text-center">main</div>;
 };
 
 export default Main;
