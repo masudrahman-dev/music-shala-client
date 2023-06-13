49ba0a821148eea3b1826d5923d26a1da940a86a
masudrahman-dev <masudrahman.dev@gmail>
2023-06-13 07:11:52 +0600

    update icon and active link

--- src/layout/Dashboard/InstrctorDashboard/InstructorDashboard.jsx
+++ src/layout/Dashboard/InstrctorDashboard/InstructorDashboard.jsx
@@ -1,3 +1,4 @@
+import { ListPlus, Plus } from "@phosphor-icons/react";
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
@@ -5,45 +6,38 @@ const InstructorDashboard = () => {
   return (
     <>
       <li>
-        <Link
+        <NavLink
           to="/dashboard/instructor/add-class"
           className="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
         >
-          <svg
-            className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
-            <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
-          </svg>
-          <span className="flex-1 ml-3 text-left whitespace-nowrap">
-            Add Class
-          </span>
-        </Link>
+          {({ isActive }) => (
+            <div
+              className={`${
+                isActive ? "text-fuchsia-500 font-bold " : ""
+              }  text-left whitespace-nowrap flex items-center justify-center gap-3`}
+            >
+              <Plus size={28} />
+              <p> Add Class</p>
+            </div>
+          )}
+        </NavLink>
       </li>
+
       <li>
         <NavLink
           to="instructor/my-classes"
           className="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
         >
-          <svg
-            className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-            <path
-              fillRule="evenodd"
-              d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span className="flex-1 ml-3 text-left whitespace-nowrap">
-            My Classes
-          </span>
+          {({ isActive }) => (
+            <div
+              className={`${
+                isActive ? "text-fuchsia-500 font-bold " : ""
+              }  text-left whitespace-nowrap flex items-center justify-center gap-3`}
+            >
+              <ListPlus size={28} />
+              <p> My Classes</p>
+            </div>
+          )}
         </NavLink>
       </li>
     </>
--- src/layout/Dashboard/StudentDashboard/StudentDashboard.jsx
+++ src/layout/Dashboard/StudentDashboard/StudentDashboard.jsx
@@ -1,54 +1,53 @@
+import { ArrowCircleRight, Basket } from "@phosphor-icons/react";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const StudentDashboard = () => {
+  <>
+    <NavLink to="/">
+      {({ isActive }) => (
+        <span className={isActive ? "text-fuchsia-500 font-bold" : ""}>
+          Home
+        </span>
+      )}
+    </NavLink>
+  </>;
   return (
     <>
       <li>
-        <Link
+        <NavLink
           to="/dashboard/student/selected-classes"
           className="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
         >
-          <svg
-            className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span className="flex-1 ml-3 text-left whitespace-nowrap">
-            Selected Classes
-          </span>
-        </Link>
+          {({ isActive }) => (
+            <div
+              className={`${
+                isActive ? "text-fuchsia-500 font-bold " : ""
+              }  text-left whitespace-nowrap flex items-center justify-center gap-3`}
+            >
+              <ArrowCircleRight size={28} />
+              <p> Selected Classes</p>
+            </div>
+          )}
+        </NavLink>
       </li>
+
       <li>
-        <Link
+        <NavLink
           to="/dashboard/student/enrolled-classes"
           className="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-          aria-controls="dropdown-sales"
-          data-collapse-toggle="dropdown-sales"
         >
-          <svg
-            className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span className="flex-1 ml-3 text-left whitespace-nowrap">
-            Enrolled Classes
-          </span>
-        </Link>
+          {({ isActive }) => (
+            <div
+              className={`${
+                isActive ? "text-fuchsia-500 font-bold " : ""
+              }  text-left whitespace-nowrap flex items-center justify-center gap-3`}
+            >
+              <Basket size={28} />
+              <p> Enrolled Classes</p>
+            </div>
+          )}
+        </NavLink>
       </li>
     </>
   );
